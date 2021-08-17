b93edf7fad13db055ce63ac79b7c144bfd4bb6da
e.gracheva <e.gracheva@sweeftdigital.com>
2021-08-17 12:03:21 +0400

    migration

--- src/CarSales/Data/ApplicationDbContext.cs
+++ src/CarSales/Data/ApplicationDbContext.cs
@@ -12,8 +12,8 @@ namespace CarSales.Data
         {
         }
 
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
 
     }
 }
---
+++ src/CarSales/Migrations/20210817075755_Initial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarSales.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Make = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CarNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManufactureYear = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VinCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SaleStartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SaleEndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonalNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
